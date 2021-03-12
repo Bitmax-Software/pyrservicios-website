ad98d1d402a9477acb9b3eed154d9c8efc447056
Piton007 <josemoraleswatanabe@gmail.com>
2021-03-11 19:23:58 -0500

    add color transitions of navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
+   
+const navbar = $("nav")
 window.addEventListener("scroll",function(){
-    console.log("hello")
+    const offset = Math.max( $("html").scrollTop(), $("body").scrollTop() )
+    console.log(offset)
+    if (offset == 0) {
+        setAbsoluteNavbarPos(navbar)
+    }else{
+       setFixedNavbarPos(navbar)
+    } 
 })
+
+function setFixedNavbarPos(navbar){
+    navbar.removeClass("position-absolute top-0")
+    navbar.removeClass("navbar-dark")
+    navbar.removeClass("bg-transparent")
+    navbar.addClass("bg-light")
+    navbar.addClass("navbar-light")
+    navbar.addClass("position-fixed")
+}
+
+function setAbsoluteNavbarPos(navbar){
+
+    navbar.removeClass("position-fixed")
+    navbar.removeClass("navbar-light")
+    navbar.removeClass("bg-light")
+    navbar.addClass("bg-transparent")
+    navbar.addClass("navbar-dark")
+    navbar.addClass("position-absolute top-0")
+}
