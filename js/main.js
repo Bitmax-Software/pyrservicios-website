d12f14e68ba256a89a8ec5550b73941bbdd6811e
Piton007 <josemoraleswatanabe@gmail.com>
2021-03-12 19:47:54 -0500

    add navbar shadows

--- js/main.js
+++ js/main.js
@@ -11,20 +11,11 @@ window.addEventListener("scroll",function(){
 })
 
 function setFixedNavbarPos(navbar){
-    navbar.removeClass("position-absolute top-0")
-    navbar.removeClass("navbar-dark")
-    navbar.removeClass("bg-transparent")
-    navbar.addClass("bg-light")
-    navbar.addClass("navbar-light")
-    navbar.addClass("position-fixed")
+    navbar.removeClass("bg-transparent navbar-dark position-absolute top-0")
+    navbar.addClass("bg-light navbar-light position-fixed material-shadow")
 }
 
 function setAbsoluteNavbarPos(navbar){
-
-    navbar.removeClass("position-fixed")
-    navbar.removeClass("navbar-light")
-    navbar.removeClass("bg-light")
-    navbar.addClass("bg-transparent")
-    navbar.addClass("navbar-dark")
-    navbar.addClass("position-absolute top-0")
+    navbar.removeClass("bg-light navbar-light position-fixed material-shadow")
+    navbar.addClass("bg-transparent navbar-dark position-absolute top-0")
 }
