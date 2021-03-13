965f8e3145ac296a9318b2b87c211e98498749ce
Piton007 <josemoraleswatanabe@gmail.com>
2021-03-12 19:31:46 -0500

    add contact us section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
    
 const navbar = $("nav")
 
+    var forms = document.getElementsByClassName('needs-validation')
+    
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        
+        event.preventDefault();
+        if (form.checkValidity() === false) {
+           
+          event.stopPropagation();
+          form.classList.add('was-validated');
+        }else{
+           
+            sendEmail()
+            form.classList.remove('was-validated');
+            $("form")[0].reset()
+        }
+       
+      }, false);})
+
+
 function sendEmail(){
+    const name = $("#contact_name")
+    const email = $("#contact_email")
+    const phone = $("#contact_cel")
+    const description = $("#contact_msg")
+   
     Email.send({
-        SecureToken : "ab542751-97de-40aa-8771-0d38a82f2920",
-        To : 'josemoraleswatanabe@gmail.com',
-        From : "you@isp.com",
-        Subject : "This is the subject",
-        Body : "And this is the body"
+        SecureToken : "cd6ff455-9bf1-4109-90f0-7e24530a9a64",
+        To : 'JoseMoWa@gmail.com',
+        From : email.val(),
+        Subject : "Consulta de solicitud de servicios PyR de "+ name.val(),
+        Body : getPhone(phone) + description.val()
     }).then(
       message => alert(message)
     );
@@ -39,4 +65,8 @@ function setAbsoluteNavbarPos(navbar){
     navbar.addClass("bg-transparent")
     navbar.addClass("navbar-dark")
     navbar.addClass("position-absolute top-0")
+}
+
+function getPhone(phone){
+    return (!!phone.text()) ? "Número de contacto: " + phone.text() + "\n" : "" 
 }
