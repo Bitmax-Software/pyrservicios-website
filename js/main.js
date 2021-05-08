8ea43db8fac42111fc4e440dd5d226531c443da7
Piton007 <josemoraleswatanabe@gmail.com>
2021-05-08 11:50:16 -0500

    fix scroll movement in landing

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@
       });
 var elems = document.querySelectorAll('.m-carousel');
 
+var offset = 60
+
+$('.navbar-nav li a').click(function(event) {
+  event.preventDefault();
+  $($(this).attr('href'))[0].scrollIntoView();
+  scrollBy(0, -offset);
+});
+
 $('.carousel').carousel({
   interval: 3000
 });
